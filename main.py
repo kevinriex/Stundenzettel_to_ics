60c3eb50f0a15e08bbf836c86a2823fca85da57d
Kevin Riexinger <info@kevinriexinger.de>
2022-07-18 23:34:48 +0200

    working without checks

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import terminaltables as tt
+from sys import argv
 
-def readfile():
-    f = open("data.csv")
+def readfile(file_name):
+    f = open(file_name)
     data = f.readlines()
     f.close()
     data.pop(0)
@@ -19,21 +20,21 @@ def generate_table_data(data):
             tt_data.append([a,b,c,d])
     return tt_data
 
-def write_data_to_txt(table):
-    d = open("out.txt", "w")
+def write_data_to_txt(table, file_name):
+    d = open(file_name, "w")
     d.write(table)
     d.close()
 
-def write_data_to_ics(table):
+def write_data_to_ics(table, file_name, eventname, location, description):
     table.pop(0)
-    i = open("out.ics", "w")
+    i = open(file_name, "w")
 
     i.write("BEGIN:VCALENDAR\n")
     i.write("VERSION:2.0\n")
     i.write("CALSCALE:GREGORIAN\n")
     for x in table:
         i.write("BEGIN:VEVENT\n")
-        i.write("SUMMARY:KomMITT Arbeitstag\n")
+        i.write(f"SUMMARY:{eventname}\n")
 
         startdata = str(x[0][6:10]) + str(x[0][3:5]) + str(x[0])[0:2] + "T" + str(x[1])[0:2] + str(x[1])[3:5]
         enddata = str(x[0][6:10]) + str(x[0][3:5]) + str(x[0])[0:2] + "T" + str(x[2])[0:2] + str(x[2])[3:5]
@@ -42,10 +43,10 @@ def write_data_to_ics(table):
         i.write(start)
         i.write(end)
         
-        i.write("LOCATION:Kaiserswerther Str. 85, 40878 Ratingen\n")
+        i.write(f"LOCATION:{location}\n")
         
         pausedata = x[3] if x[3] != "" else "///"
-        pause= f"DESCRIPTION:Ein normaler Arbeitstag bei der KomMITT. Pause {pausedata}\n"
+        pause= f"DESCRIPTION:{description} Pause {pausedata}\n"
         i.write(pause)
 
         i.write("STATUS:CONFIRMED\n")
@@ -55,10 +56,38 @@ def write_data_to_ics(table):
 
     i.close()
 
-tt_data = generate_table_data(readfile())
-table = tt.AsciiTable(tt_data,"Stundenzettel")
-# print(tt_data)
-print(table.table)
-write_data_to_txt(table.table)
-write_data_to_ics(tt_data)
+def main():
+    userdata = start()
+    if userdata != None:
+        tt_data = generate_table_data(readfile(userdata[0]))
+        table = tt.AsciiTable(tt_data,"Stundenzettel")
+        # print(tt_data)
+        print(table.table)
+        write_data_to_txt(table.table,userdata[2])
+        write_data_to_ics(tt_data,userdata[1],userdata[3],userdata[4],userdata[5])
+    else:
+        man()
 
+def start():
+    if len(argv) != 1:
+        if argv[1] == "/?" or "help":
+            man()
+        else:
+            if len(argv) < 6:
+                print("You have to provide all arguments...")
+            else:
+                incsv = argv[1]
+                outics = argv[2]
+                outtxt = argv[3]
+                eventname = argv[4]
+                location = argv[5]
+                description = argv[6]
+                return [incsv,outics,outtxt,eventname,location,description]
+    else:
+        print("You have to provide arguments...")
+        return None
+
+def man():
+    print("Manual")
+
+main()
